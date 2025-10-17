f56ae0253cf4deca376bec247b935322fcd6ad3a
TastyPommesLul <ernstd.privat@gmail.com>
2025-10-17 08:18:46 +0200

    e

--- src/main/java/net/tastypommeslul/darkDupe/commands/DupeCommand.java
+++ src/main/java/net/tastypommeslul/darkDupe/commands/DupeCommand.java
@@ -19,54 +19,54 @@ public class DupeCommand extends Command {
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
-        final ArrayList<ItemStack> items = new ArrayList<>();
+        final ArrayList<Material> items = new ArrayList<>();
         if (!(sender instanceof Player player)) {
           sender.sendMessage("This command can only be run by players.");
           return false;
         }
-        ItemStack currentItem = player.getInventory().getItemInMainHand();
-        if (currentItem.getType() == Material.AIR) {
+        Material currentItem = player.getInventory().getItemInMainHand().getType();
+        if (currentItem == Material.AIR) {
           sender.sendMessage("You must be holding an item to dupe.");
           return false;
         }
-        boolean isShulker = currentItem.getType() == Material.SHULKER_BOX
-                || currentItem.getType() == Material.RED_SHULKER_BOX
-                || currentItem.getType() == Material.BLUE_SHULKER_BOX
-                || currentItem.getType() == Material.BLACK_SHULKER_BOX
-                || currentItem.getType() == Material.BROWN_SHULKER_BOX
-                || currentItem.getType() == Material.CYAN_SHULKER_BOX
-                || currentItem.getType() == Material.GRAY_SHULKER_BOX
-                || currentItem.getType() == Material.GREEN_SHULKER_BOX
-                || currentItem.getType() == Material.LIGHT_BLUE_SHULKER_BOX
-                || currentItem.getType() == Material.LIGHT_GRAY_SHULKER_BOX
-                || currentItem.getType() == Material.LIME_SHULKER_BOX
-                || currentItem.getType() == Material.MAGENTA_SHULKER_BOX
-                || currentItem.getType() == Material.ORANGE_SHULKER_BOX
-                || currentItem.getType() == Material.PINK_SHULKER_BOX
-                || currentItem.getType() == Material.PURPLE_SHULKER_BOX
-                || currentItem.getType() == Material.WHITE_SHULKER_BOX
-                || currentItem.getType() == Material.YELLOW_SHULKER_BOX;
+        boolean isShulker = currentItem == Material.SHULKER_BOX
+                || currentItem == Material.RED_SHULKER_BOX
+                || currentItem == Material.BLUE_SHULKER_BOX
+                || currentItem == Material.BLACK_SHULKER_BOX
+                || currentItem == Material.BROWN_SHULKER_BOX
+                || currentItem == Material.CYAN_SHULKER_BOX
+                || currentItem == Material.GRAY_SHULKER_BOX
+                || currentItem == Material.GREEN_SHULKER_BOX
+                || currentItem == Material.LIGHT_BLUE_SHULKER_BOX
+                || currentItem == Material.LIGHT_GRAY_SHULKER_BOX
+                || currentItem == Material.LIME_SHULKER_BOX
+                || currentItem == Material.MAGENTA_SHULKER_BOX
+                || currentItem == Material.ORANGE_SHULKER_BOX
+                || currentItem == Material.PINK_SHULKER_BOX
+                || currentItem == Material.PURPLE_SHULKER_BOX
+                || currentItem == Material.WHITE_SHULKER_BOX
+                || currentItem == Material.YELLOW_SHULKER_BOX;
 
-        logic(currentItem, items, player, isShulker);
+        logic(player.getInventory().getItemInMainHand(), items, player, isShulker);
 
         items.clear();
         return true;
     }
 
-    void logic(ItemStack currentItem, ArrayList<ItemStack> items, Player sender, boolean isShulker) {
+    void logic(ItemStack currentItem, ArrayList<Material> items, Player sender, boolean isShulker) {
         if (isShulker) {
             List<ItemStack> contents = currentItem.getData(DataComponentTypes.CONTAINER).contents();
             for (ItemStack item : contents) {
-                if (BlockedItems.items.contains(item)) {
-                    items.add(item);
+                if (BlockedItems.items.contains(item.getType())) {
+                    items.add(item.getType());
                     sender.sendMessage(Component.text("Found blocked items in your shulker box."));
                     sender.sendMessage(Component.text("Remove them and try again."));
                     return;
                 }
             }
         } else {
-            if (BlockedItems.items.contains(currentItem)) {
-                items.add(currentItem);
+            if (BlockedItems.items.contains(currentItem.getType())) {
+                items.add(currentItem.getType());
                 sender.sendMessage(Component.text("Blocked item. " + currentItem.getType().name()));
                 return;
             }
