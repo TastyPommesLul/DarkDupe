8a321efcd9145464edbc68fbf519e90cd380ebc5
TastyPommesLul <ernstd.privat@gmail.com>
2025-10-17 06:11:51 +0200

    Initial commit

---
+++ src/main/kotlin/net/tastypommeslul/darkDupe/DarkDupe.kt
@@ -0,0 +1,14 @@
+package net.tastypommeslul.darkDupe
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DarkDupe : JavaPlugin() {
+
+  override fun onEnable() {
+    // Plugin startup logic
+  }
+
+  override fun onDisable() {
+    // Plugin shutdown logic
+  }
+}
