c9ba19878aaceec111af556343eb8698217fb0e4
TastyPommesLul <ernstd.privat@gmail.com>
2025-11-19 21:20:39 +0100

    e

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "net.tastypommeslul"
-version = "1.2"
+version = "1.3"
 
 repositories {
   mavenCentral()
--- src/main/java/net/tastypommeslul/darkDupe/commands/DupeCommand.java
+++ src/main/java/net/tastypommeslul/darkDupe/commands/DupeCommand.java
@@ -1,6 +1,10 @@
 package net.tastypommeslul.darkDupe.commands;
 
 import io.papermc.paper.datacomponent.DataComponentTypes;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.tastypommeslul.darkDupe.BlockedItems;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -10,6 +14,8 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DupeCommand extends Command {
     public DupeCommand() {
@@ -36,6 +42,23 @@ public class DupeCommand extends Command {
         return true;
     }
 
+    boolean hasItemTag(ItemStack currentItem) {
+        AtomicBoolean isUndupeable = new AtomicBoolean(false);
+        Style style = Style.empty().decorations(Map.of(
+                TextDecoration.BOLD, TextDecoration.State.FALSE,
+                TextDecoration.ITALIC, TextDecoration.State.FALSE,
+                TextDecoration.OBFUSCATED, TextDecoration.State.FALSE,
+                TextDecoration.STRIKETHROUGH, TextDecoration.State.FALSE,
+                TextDecoration.UNDERLINED, TextDecoration.State.FALSE
+        ));
+        if (currentItem.getItemMeta().hasLore()) {
+            currentItem.getItemMeta().lore().forEach(text -> text.children().forEach(child -> {
+                isUndupeable.set(child.equals(Component.text("Undupeable").style(style).color(NamedTextColor.GRAY)));
+            }));
+        }
+        return isUndupeable.get();
+    }
+
     void logic(ItemStack currentItem, ArrayList<Material> items, Player sender, boolean isShulkerOrBundle) {
         if (BlockedItems.bundles.contains(currentItem.getType())) {
             sender.sendRichMessage("<red><bold>Bundles can't be duped.");
@@ -44,20 +67,21 @@ public class DupeCommand extends Command {
         if (isShulkerOrBundle) {
             List<ItemStack> contents = currentItem.getData(DataComponentTypes.CONTAINER).contents();
             for (ItemStack item : contents) {
-                if (BlockedItems.items.contains(item.getType()) || BlockedItems.bundles.contains(item.getType())) {
+                if (BlockedItems.items.contains(item.getType()) || BlockedItems.bundles.contains(item.getType()) || hasItemTag(item)) {
                     items.add(item.getType());
-                    sender.sendRichMessage("Found blocked item in your shulker box. <red><bold>" + item.getType().name());
-                    sender.sendRichMessage("Remove them and try again.");
+                    sender.sendRichMessage("Found blocked item in your shulker box.");
+                    sender.sendRichMessage("Remove <red><bold>" + item.getType().name().toLowerCase() + "</bold></red> them and try again.");
                     return;
                 }
             }
         } else {
-            if (BlockedItems.items.contains(currentItem.getType())) {
+            if (BlockedItems.items.contains(currentItem.getType()) || hasItemTag(currentItem)) {
                 items.add(currentItem.getType());
-                sender.sendRichMessage("Blocked item. <red><bold>" + currentItem.getType().name());
+                sender.sendRichMessage("Blocked item. <red><bold>" + currentItem.getType().name().toLowerCase());
                 return;
             }
         }
+        System.out.println("Duped?");
         sender.getInventory().addItem(currentItem);
         sender.sendRichMessage("<green><bold>Duping Completed.");
         items.clear();
